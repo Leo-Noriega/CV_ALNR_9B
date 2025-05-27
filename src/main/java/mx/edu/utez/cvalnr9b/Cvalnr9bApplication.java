89ff5ba3282fcedec01d9faed83ac014685f4c23
Leo-Noriega <leonoriega100@gmail.com>
2025-05-27 15:50:38 -0600

    chore: create project

---
+++ src/main/java/mx/edu/utez/cvalnr9b/Cvalnr9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvalnr9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvalnr9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvalnr9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvalnr9b/Cvalnr9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvalnr9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvalnr9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
